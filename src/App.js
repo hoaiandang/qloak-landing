95b505f171aa9fff749b191f23aeeae214ba8a10
Hoaian Dang <hoaiandang@berkeley.edu>
2019-06-15 15:51:31 -0700

    implement css styling instead of inline styling to allow for media queries

--- src/App.js
+++ src/App.js
@@ -6,14 +6,61 @@ import Page from './components/Page.js'
 const content = (<p style={{margin: 20}}>Qloak</p>)
 const children = (<p style={{margin: 20}}>(^.^)/</p>)
 
-const App = () => (
-    <div className="App">
-      <Page content={content} children={children}/>
-      <Page color={('#F7F5F2')}/>
-      <Page content={content} children={children}/>
-      <Page color={('#F7F5F2')}/>
-      <Page content={content} children={children}/>
+const rowContainer = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+}
+
+const buttonStyle = {
+  backgroundColor: '#01B0A5',
+  color: 'white',
+  fontFamily: 'Ubuntu',
+  fontSize: 20,
+  fontWeight: 'bold',
+  padding: 10,
+  paddingLeft: 40,
+  paddingRight: 40,
+  margin: 10,
+  borderRadius: 40,
+}
+
+const PageOne = (
+  <div className='column-container'
+    style={{height: '50vh',
+            justifyContent: 'space-around',
+            alignItems: 'flex-start'}}>
+    <div>
+      <div style={rowContainer}>
+        <h1 >Qloak</h1><h1 style={{color: '#01B0A5'}}>.</h1>
+      </div>
+      <h5 style={{margin: 0}}>Find your place</h5>
     </div>
+    <div style={rowContainer}>
+      <div className='button'>Sign Up</div>
+      <div className='button'>Learn More</div>
+    </div>
+  </div>)
+
+const App = () => (
+
+    <div className="App">
+      <Page>{PageOne}</Page>
+      <Page color={('#F7F7F7')}></Page>
+      <Page content={content}/>
+      <Page color={('#F7F7F7')}/>
+      <Page content={content}/>
+      <Page color={('#F7F7F7')}/>
+    </div>
+
+    /*
+    <div>
+      <div className="page">slfdjkllksdljkfsdkljsf;lkaja;lsdfjl;ksdjfl;kjsdaf;ljafl;jfdl;kjfks</div>
+      <div className='page'>sldffld</div>
+      <div className="page"/>
+      <div className="page"/>
+    </div>
+    */
 );
 
 
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import '../App.css';
 
 function Page(props) {
   const pageStyle = {
     width: '100vw',
     height: '100vh',
     backgroundColor: 'white',
-    color: 'black',
+    color: '#707070',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'row',
-    backgroundColor: props.color
+    backgroundColor: props.color,
+    fontFamily: 'Ubuntu',
+    fontSize: 80,
+    fontWeight: 'bold',
+
+  }
+  const colorStyle = {
+    backgroundColor: props.color,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    fontFamily: 'Ubuntu',
+    color: '#707070',
   }
   return (
+    /*
     <div style={pageStyle}>
+      {props.content}
+      {props.children}
+    </div>
+    */
+    <div className='page'>
       {props.content}
       {props.children}
     </div>
